cc4c2d3c272a3a6be8d9bd5aac548d56b9515210
Alex <mendozaalex24@gmail.com>
2025-04-09 21:19:47 -0300

    project creation and configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='w-full h-full text-9xl font-bold flex justify-center items-center'>Proximamente Mathflix!</h1>
+    </>
+  )
+}
+
+export default App
